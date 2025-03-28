ad3b49aa9f4c695451de7f35e7e513fd49accfd3
DuyBinh <duybinh10012004@gmail.com>
2025-03-28 13:59:33 +0700

    submit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { LocalNotifications } from '@capacitor/local-notifications';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule
+  ]
 })
 export class HomePage {
   constructor() {}
+  currentTime: string = '';
+   public async showNotification(): Promise<void> {
+      const now = new Date();
+      this.currentTime = now.toLocaleTimeString();
+      await LocalNotifications.schedule({
+        notifications: [
+          {
+            title: 'Thông báo',
+            body: `Thời gian hiện tại là : $this.currentTime `,
+            id: 1,
+            schedule: { at: new Date(Date.now() + 1000) },
+            sound: undefined,
+            attachments: undefined,
+            actionTypeId: '',
+            extra: {}
+          }
+        ]
+      });
+    }
+
+   public async shareCurrentTime(): Promise<void> {
+      const now = new Date();
+      this.currentTime = now.toLocaleTimeString();
+      await Share.share({
+        title: 'Thời gian hiện tại',
+        text: `Thời gian hiện tại: ${this.currentTime}`,
+        dialogTitle: 'Chia sẻ thời gian hiện tại'
+      });
+    }
 }
